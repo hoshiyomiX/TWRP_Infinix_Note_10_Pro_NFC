77a3ce4dd7862ebb0d5c0d1b98a4ee6430f0e878
Miichon <Zksync@Miichon.ovh>
2023-04-26 05:49:11 +0700

    clean up
    Signed-off-by: Miichon <Zksync@Miichon.ovh>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,7 +138,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Crypto
 TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_CRYPTO_FBE := true
 
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
